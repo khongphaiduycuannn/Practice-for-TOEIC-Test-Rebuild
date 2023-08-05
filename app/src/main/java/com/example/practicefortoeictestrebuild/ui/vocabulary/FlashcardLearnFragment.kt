aa17ea1b2d70e1ba2045f30fe15c912dee4b37a9
sadnessismybestestfriend <ndmquan1010@gmail.com>
2023-08-05 15:10:05 +0700

    Add UI Flashcard topic: 1. Flashcard start fragment. 2. Flashcard learn fragment.

---
+++ app/src/main/java/com/example/practicefortoeictestrebuild/ui/vocabulary/FlashcardLearnFragment.kt
@@ -0,0 +1,20 @@
+package com.example.practicefortoeictestrebuild.ui.vocabulary
+
+import com.example.practicefortoeictestrebuild.base.BaseFragment
+import com.example.practicefortoeictestrebuild.databinding.FragmentFlashcardLearnBinding
+
+class FlashcardLearnFragment :
+    BaseFragment<FragmentFlashcardLearnBinding>(FragmentFlashcardLearnBinding::inflate) {
+
+    override fun initData() {
+
+    }
+
+    override fun handleEvent() {
+
+    }
+
+    override fun bindData() {
+
+    }
+}
---
+++ app/src/main/java/com/example/practicefortoeictestrebuild/ui/vocabulary/FlashcardStartFragment.kt
@@ -0,0 +1,20 @@
+package com.example.practicefortoeictestrebuild.ui.vocabulary
+
+import com.example.practicefortoeictestrebuild.base.BaseFragment
+import com.example.practicefortoeictestrebuild.databinding.FragmentFlashcardStartBinding
+
+class FlashcardStartFragment :
+    BaseFragment<FragmentFlashcardStartBinding>(FragmentFlashcardStartBinding::inflate) {
+
+    override fun initData() {
+
+    }
+
+    override fun handleEvent() {
+
+    }
+
+    override fun bindData() {
+
+    }
+}
