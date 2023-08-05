6bc3b07618aa52059ee93bdd8454647aaeaa7fc4
sadnessismybestestfriend <ndmquan1010@gmail.com>
2023-08-05 15:28:04 +0700

    Add loading animation.

---
+++ app/src/main/java/com/example/practicefortoeictestrebuild/utils/DialogExtension.kt
@@ -0,0 +1,26 @@
+package com.example.practicefortoeictestrebuild.utils
+
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.Gravity
+import android.view.WindowManager
+import com.example.practicefortoeictestrebuild.databinding.DialogLoadingBinding
+
+fun Dialog.startLoading(stopFlag: Boolean = false) {
+    val binding = DialogLoadingBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+    window?.apply {
+        setLayout(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.WRAP_CONTENT
+        )
+        setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        attributes.apply {
+            gravity = Gravity.CENTER
+        }
+        setDimAmount(0.5F)
+    }
+    setCancelable(stopFlag)
+    show()
+}
