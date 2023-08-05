80eba000aad07063c8f62cbb390e53d58e86c49a
sadnessismybestestfriend <ndmquan1010@gmail.com>
2023-08-05 09:43:20 +0700

    Add UI lesson fragment.

---
+++ app/src/main/java/com/example/practicefortoeictestrebuild/ui/document/LessonFragment.kt
@@ -0,0 +1,19 @@
+package com.example.practicefortoeictestrebuild.ui.document
+
+import com.example.practicefortoeictestrebuild.base.BaseFragment
+import com.example.practicefortoeictestrebuild.databinding.FragmentLessonBinding
+
+class LessonFragment : BaseFragment<FragmentLessonBinding>(FragmentLessonBinding::inflate) {
+
+    override fun initData() {
+
+    }
+
+    override fun handleEvent() {
+
+    }
+
+    override fun bindData() {
+
+    }
+}
