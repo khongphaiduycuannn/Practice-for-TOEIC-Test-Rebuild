cbdeff0a228f25219d46276681eac23116240788
sadnessismybestestfriend <ndmquan1010@gmail.com>
2023-08-05 09:36:33 +0700

    Add UI list course fragment.

---
+++ app/src/main/java/com/example/practicefortoeictestrebuild/ui/course/CourseActivity.kt
@@ -0,0 +1,19 @@
+package com.example.practicefortoeictestrebuild.ui.course
+
+import com.example.practicefortoeictestrebuild.base.BaseActivity
+import com.example.practicefortoeictestrebuild.databinding.ActivityCourseBinding
+
+class CourseActivity : BaseActivity<ActivityCourseBinding>(ActivityCourseBinding::inflate) {
+
+    override fun initData() {
+
+    }
+
+    override fun handleEvent() {
+
+    }
+
+    override fun bindData() {
+
+    }
+}
---
+++ app/src/main/java/com/example/practicefortoeictestrebuild/ui/course/CourseFragment.kt
@@ -0,0 +1,19 @@
+package com.example.practicefortoeictestrebuild.ui.course
+
+import com.example.practicefortoeictestrebuild.base.BaseFragment
+import com.example.practicefortoeictestrebuild.databinding.FragmentCourseBinding
+
+class CourseFragment : BaseFragment<FragmentCourseBinding>(FragmentCourseBinding::inflate) {
+
+    override fun initData() {
+
+    }
+
+    override fun handleEvent() {
+
+    }
+
+    override fun bindData() {
+
+    }
+}
