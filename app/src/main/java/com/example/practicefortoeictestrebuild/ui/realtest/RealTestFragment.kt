fca0d9e8eb27c3922b5fb6b5322a57d74a6f9205
sadnessismybestestfriend <ndmquan1010@gmail.com>
2023-08-10 01:40:09 +0700

    Add UI real test.

---
+++ app/src/main/java/com/example/practicefortoeictestrebuild/ui/realtest/RealTestFragment.kt
@@ -0,0 +1,18 @@
+package com.example.practicefortoeictestrebuild.ui.realtest
+
+import com.example.practicefortoeictestrebuild.base.BaseFragment
+import com.example.practicefortoeictestrebuild.databinding.FragmentRealTestBinding
+
+class RealTestFragment : BaseFragment<FragmentRealTestBinding>(FragmentRealTestBinding::inflate) {
+    override fun initData() {
+
+    }
+
+    override fun handleEvent() {
+
+    }
+
+    override fun bindData() {
+
+    }
+}
---
+++ app/src/main/java/com/example/practicefortoeictestrebuild/ui/realtest/RealTestResultFragment.kt
@@ -0,0 +1,21 @@
+package com.example.practicefortoeictestrebuild.ui.realtest
+
+import com.example.practicefortoeictestrebuild.base.BaseFragment
+import com.example.practicefortoeictestrebuild.databinding.FragmentRealTestBinding
+import com.example.practicefortoeictestrebuild.databinding.FragmentRealTestResultBinding
+
+class RealTestResultFragment :
+    BaseFragment<FragmentRealTestResultBinding>(FragmentRealTestResultBinding::inflate) {
+
+    override fun initData() {
+
+    }
+
+    override fun handleEvent() {
+
+    }
+
+    override fun bindData() {
+
+    }
+}
